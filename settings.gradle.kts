48dcc92c57d1c1a9cd6319f28d9719bbde1b1fe5
Hairul008 <hairullabri5@gmail.com>
2023-10-18 00:31:27 +0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Aplikasi Sederhana F55122008"
+include(":app")
+ 
